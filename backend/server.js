79edbe695326eaae05f72a1c9864541c2d1220c8
Aygün Bayır <turkoaygun@gmail.com>
2023-07-16 23:16:30 +0300

    starter project files

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const workoutRoutes = require('./routes/workouts');
+
+// dotenv
+dotenv.config();
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => console.log(error));
